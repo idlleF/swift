2d42cf4c7f3fb58f3e90cdd1707e790b34587a8f
Macbook Pro <macbookpro@MacBook-Pro-2.local>
2018-10-21 00:55:17 +0300

    Add navigation

--- swift-swoosh/swift-swoosh/ViewController.swift
+++ swift-swoosh/swift-swoosh/ViewController.swift
@@ -18,5 +18,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
 }
 
